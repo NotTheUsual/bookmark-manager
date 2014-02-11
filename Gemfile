b6c553992fee0d9db364a330e74f02c7895ec7a1
James Hunter <james.hunter@me.com>
2014-02-11 11:32:10 +0000

    Set up Postgres

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 ruby '2.1.0'
 
 gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 
 group :development, :test do
   gem "rspec"
--- server.rb
+++ server.rb
@@ -1,4 +1,11 @@
+env = ENV['RACK_ENV'] || "development"
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
 require 'sinatra/base'
+require './lib/link'
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
   get '/' do
