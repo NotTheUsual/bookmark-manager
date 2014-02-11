3d0afd753bdc27adb16e3c983869b371f94a48ef
James Hunter <james.hunter@me.com>
2014-02-11 12:15:43 +0000

    Added ability to list links on the homepage

--- server.rb
+++ server.rb
@@ -1,16 +1,19 @@
+env = ENV['RACK_ENV'] || "development"
+
 require 'data_mapper'
 require 'sinatra/base'
-require './lib/link'
 
-env = ENV['RACK_ENV'] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :index
   end
 
   # start the server if ruby file executed directly
