4c527ff282668e8f2af0c9b588b27d0d611ba5df
James Hunter <james.hunter@me.com>
2014-02-12 17:05:20 +0000

    Condensed Controller requirement statements

--- app/server.rb
+++ app/server.rb
@@ -2,13 +2,9 @@ env = ENV['RACK_ENV'] || "development"
 
 require 'data_mapper'
 require 'sinatra/base'
-require 'sinatra/partial'
-require 'rack-flash'
 require_relative 'db_config'
-require_relative 'helpers/server_helpers'
-require_relative 'controllers/users'
-require_relative 'controllers/links'
-require_relative 'controllers/authentication'
+
+Dir.glob(File.join(File.dirname(__FILE__), 'controllers', '*.rb'), &method(:require))
 
 class BookmarkManager < Sinatra::Base
 
