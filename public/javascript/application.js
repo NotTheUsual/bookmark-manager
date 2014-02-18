0a4a13d6fd295903371e66cd4b735f4048b60463
James Hunter <james.hunter@me.com>
2014-02-18 15:50:12 +0000

    Added flash messages for favourites

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -5,7 +5,21 @@ function addFavouritesHandler() {
     var newOpacity = favourited ? 0 : 1;
     $(link).data('favourited', !favourited);
     $(this).animate({opacity: newOpacity}, 200);
+    showLinkFavouritedNotice(link);
   });
+}
+
+function showLinkFavouritedNotice(link) {
+  var favourited = !!$(link).data('favourited');
+  var name = $(link).find('.title').text();
+  var message = favourited ?
+                name + ' was added to favourites' :
+                name + ' was remvoed from favourites';
+  var $flash = $("<div></div>").addClass('flash notice fixed').html(message);
+  $flash.appendTo('#flash-container');
+  window.setTimeout(function(){
+    $flash.fadeOut();
+  }, 3000);
 }
 
 $(function () {
