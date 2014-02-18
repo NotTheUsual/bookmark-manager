8bd83b5dbd5ab3d4e504c16340668aac6a06eac6
James Hunter <james.hunter@me.com>
2014-02-18 15:20:08 +0000

    Added favourites stars

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,13 @@
-function animateLinks() {
-  $('.link').show(1000);
+function addFavouritesHandler() {
+  $(".star.solid").click(function (event) {
+    var link = $(this).parent();
+    var favourited = !!$(link).data('favourited');
+    var newOpacity = favourited ? 0 : 1;
+    $(link).data('favourited', !favourited);
+    $(this).animate({opacity: newOpacity}, 200);
+  });
 }
 
-$(function() {
-  animateLinks();
+$(function () {
+  addFavouritesHandler();
 });
